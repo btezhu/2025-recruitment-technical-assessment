c43f38592bac1259ca9896b7abb33363265ae710
btezhu <m.burykin@student.unsw.edu.au>
2025-02-24 22:47:09 +1100

    Tasks 1, 2, 3

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -1,24 +1,15 @@
 from dataclasses import dataclass
-from typing import List, Dict, Union
+from typing import Counter, List, Dict, Tuple, Union
 from flask import Flask, request, jsonify
 import re
 
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
-class CookbookEntry:
-	name: str
+class Recipe:
+	required_items: Counter[str]
 
 @dataclass
-class RequiredItem():
-	name: str
-	quantity: int
-
-@dataclass
-class Recipe(CookbookEntry):
-	required_items: List[RequiredItem]
-
-@dataclass
-class Ingredient(CookbookEntry):
+class Ingredient:
 	cook_time: int
 
 
@@ -28,7 +19,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook: List[CookbookEntry] = []
+cookbook: Dict[str, Recipe | Ingredient] = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -42,9 +33,11 @@ def parse():
 
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that
+regex = re.compile("[^a-zA-Z\\s]")
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	nonletter = re.compile('[^a-zA-Z\\s]')
-	return ' '.join(map(lambda x: x.capitalize(), nonletter.sub('', recipeName.replace('-', ' ').replace('_', ' ')).split())) or None
+	recipeName = recipeName.replace('-', ' ').replace('_', ' ')
+	recipeName = regex.sub('', recipeName)
+	return ' '.join(map(str.capitalize, recipeName.split())) or None
 
 
 # [TASK 2] ====================================================================
@@ -52,37 +45,78 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 @app.route('/entry', methods=['POST'])
 def create_entry():
 	data = request.json
-	for prev_entry in cookbook:
-		if prev_entry.name == data["name"]: return 'Duplicate name', 400
 
-	# Can't use match because I don't know if their machine uses 3.10+
+	name = data["name"]
+	if name in cookbook: return 'Duplicate name', 400
+
+	# Can't use match because I don't know if their machine uses Python 3.10+
+	# E.g., My Debian VPS comes with 3.9
 	if data["type"] == "recipe":
-		required_items: List[RequiredItem] = []
+		required_items: Counter[str] = Counter()
 		for item in data["requiredItems"]:
-			new_item = RequiredItem(item["name"], int(item["quantity"]))
-			for prev_item in required_items:
-				if new_item.name == prev_item.name: return 'Duplicate name', 400
-			required_items.append(new_item)
+			if item["name"] in required_items: return 'Duplicate name', 400
+			required_items[item["name"]] = item["quantity"]
 		
-		new_entry = Recipe(data["name"], required_items)
+		cookbook[name] = Recipe(required_items)
 			
 	elif data["type"] == "ingredient":
-		new_entry = Ingredient(data["name"], int(data["cookTime"]))
-		if new_entry.cook_time < 0: return 'Cannot have negative cook time!', 400
+		cook_time = data["cookTime"]
+		if cook_time < 0: return 'Negative cook time', 400
 
-	else: return 'Invalid type!', 400
+		cookbook[name] = Ingredient(cook_time)
 
-	cookbook.append(new_entry)
+	else: return 'Invalid type', 400
 
 	return '', 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
+
+def get_summary(name: str) -> Tuple[int, Counter[str]] | None:
+	if name not in cookbook: return None
+	entry = cookbook[name]
+
+	if isinstance(entry, Recipe):
+		total_time = 0
+		total_ingredients: Counter[str] = Counter()
+
+		for (item_name, quantity) in entry.required_items.items():
+			item_time_and_ingredients = get_summary(item_name)
+			if item_time_and_ingredients is None: return None
+			item_time, item_ingredients = item_time_and_ingredients
+
+			total_time += quantity * item_time
+			item_ingredients = Counter({
+				name: quantity * item_quantity 
+				for name, item_quantity in item_ingredients.items()
+			})
+			total_ingredients.update(item_ingredients)
+		
+		return total_time, total_ingredients
+
+	elif isinstance(entry, Ingredient):
+		return entry.cook_time, Counter({name: entry.cook_time})
+
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	entry_name = request.args["name"]
+	if entry_name in cookbook and isinstance(cookbook[entry_name], Ingredient):
+		return 'Ingredient', 400
+
+	entry_summary = get_summary(entry_name)
+
+	if entry_summary is None: return '', 400
+	time, ingredients = entry_summary
+
+	return {
+		"name": entry_name,
+		"cookTime": time,
+		"ingredients": [{
+			"name": ingredient,
+			"quantity": quantity
+		} for ingredient, quantity in ingredients.items()]
+	}, 200
 
 
 # =============================================================================
